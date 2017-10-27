b3bb6dd69fd6f34075574d00a1fda83a913ec463
Unknown <jreesep@gmail.com>
2017-10-27 16:38:00 -0500

    new Homework 4 project

---
+++ PerryHMK04/PerryHMK04/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PerryHMK04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.File = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.ProcessFileCheckBox = new System.Windows.Forms.CheckBox();
+            this.OutputText = new System.Windows.Forms.RichTextBox();
+            this.OutputGrouping = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // File
+            // 
+            this.File.Location = new System.Drawing.Point(5, 6);
+            this.File.Name = "File";
+            this.File.ReadOnly = true;
+            this.File.Size = new System.Drawing.Size(250, 22);
+            this.File.TabIndex = 0;
+            this.File.Click += new System.EventHandler(this.File_Click);
+            this.File.Validating += new System.ComponentModel.CancelEventHandler(this.OpenFilePath_Validating);
+            this.File.Validated += new System.EventHandler(this.OpenFilePath_Validated);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            this.errorProvider1.Icon = ((System.Drawing.Icon)(resources.GetObject("errorProvider1.Icon")));
+            // 
+            // ProcessFileCheckBox
+            // 
+            this.ProcessFileCheckBox.AutoSize = true;
+            this.ProcessFileCheckBox.Location = new System.Drawing.Point(5, 34);
+            this.ProcessFileCheckBox.Name = "ProcessFileCheckBox";
+            this.ProcessFileCheckBox.Size = new System.Drawing.Size(107, 21);
+            this.ProcessFileCheckBox.TabIndex = 1;
+            this.ProcessFileCheckBox.Text = "Process File";
+            this.ProcessFileCheckBox.UseVisualStyleBackColor = true;
+            this.ProcessFileCheckBox.CheckedChanged += new System.EventHandler(this.ProcessFileCheckBox_CheckedChanged);
+            // 
+            // OutputText
+            // 
+            this.OutputText.Location = new System.Drawing.Point(12, 84);
+            this.OutputText.Name = "OutputText";
+            this.OutputText.Size = new System.Drawing.Size(258, 99);
+            this.OutputText.TabIndex = 2;
+            this.OutputText.Text = "";
+            this.OutputText.Visible = false;
+            // 
+            // OutputGrouping
+            // 
+            this.OutputGrouping.Location = new System.Drawing.Point(5, 61);
+            this.OutputGrouping.Name = "OutputGrouping";
+            this.OutputGrouping.Size = new System.Drawing.Size(271, 130);
+            this.OutputGrouping.TabIndex = 3;
+            this.OutputGrouping.TabStop = false;
+            this.OutputGrouping.Text = "Output";
+            this.OutputGrouping.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(282, 195);
+            this.Controls.Add(this.ProcessFileCheckBox);
+            this.Controls.Add(this.OutputText);
+            this.Controls.Add(this.File);
+            this.Controls.Add(this.OutputGrouping);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "PerryHMK4";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox File;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.RichTextBox OutputText;
+        private System.Windows.Forms.CheckBox ProcessFileCheckBox;
+        private System.Windows.Forms.GroupBox OutputGrouping;
+    }
+}
+
---
+++ PerryHMK04/PerryHMK04/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PerryHMK04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void File_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void OpenFilePath_Validating(object sender, CancelEventArgs e)
+        {
+            errorProvider1.SetIconPadding((sender as TextBox), 5);
+
+            if(!IsValidPath((sender as TextBox).Text, out string errMsg))
+            {
+                e.Cancel = true;
+                (sender as TextBox).Select(0, (sender as TextBox).Text.Length);
+                errorProvider1.SetError((sender as TextBox), errMsg);
+            }
+        }
+
+        private void OpenFilePath_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError((sender as TextBox), "");
+        }
+
+        private void ProcessFileCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            System.Text.RegularExpressions.Regex numeric = new System.Text.RegularExpressions.Regex("^[0-9](\\.?[0-9])+$");
+
+            if ((sender as CheckBox).Checked)
+            {
+                using (StreamReader sr = new StreamReader(File.Text))
+                {
+                    // initialize some accumulator variables (for lines of numbers count and average)
+                    int numCount = 0;
+                    int count = 0;
+
+                    string line = sr.ReadLine();
+
+                    while (line != null) // null is returned at the end of the file
+                    {
+                        // Process
+                        if (numeric.IsMatch(line))
+                        {
+                            // process all lines that are numeric
+                            numCount++;
+                        }
+
+                        // read the next line
+                        line = sr.ReadLine();
+                        count++;
+                    }
+
+                    OutputText.Text += String.Format($"{openFileDialog1.FileName} contains {numCount} line(s) of numbers ({(double)numCount/ count:P0})");
+                }
+
+                OutputGrouping.Visible = true;
+                OutputText.Visible = true;
+            }
+            else
+            {
+                OutputText.Text += "\n";
+            }
+        }
+
+        private bool IsValidPath(string path, out string errMsg)
+        {
+            if (path == "")
+            {
+                errMsg = "A path is needed!";
+                return false;
+            }
+            else if(openFileDialog1.CheckFileExists)
+            {
+                errMsg = "";
+                return true;
+            }
+
+            errMsg = "not a file";
+            return false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = false;
+            errorProvider1.Clear();
+        }
+    }
+}
