f6001ca7e11c49855cd12d86a4751c77eee82b4b
Unknown <jreesep@gmail.com>
2017-12-25 23:35:38 -0600

    setup v6

---
+++ Database(Pokedex)/InstallerSetup/InstallerSetup1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace InstallerSetup
+{
+    [RunInstaller(true)]
+    public partial class InstallerSetup1 : Installer
+    {
+        public InstallerSetup1()
+        {
+            InitializeComponent();
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Install(IDictionary savedState)
+        {
+            showParameters();
+
+            base.Install(savedState);
+            //Add custom code here
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+            //Add custom code here
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(IDictionary savedState)
+        {
+            try
+            {
+                if (Context.Parameters["DATABASECONNECTIONPROVIDER"] == "Checked")
+                {
+                    Process.Start("AccessDatabaseEngine.exe");
+                }
+                else
+                {
+                    base.Commit(savedState);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+                base.Rollback(savedState);
+            }
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Uninstall(IDictionary savedState)
+        {
+            Process application = null;
+            foreach (var process in Process.GetProcesses())
+            {
+                if (!process.ProcessName.ToLower().Contains("Pokedex")) continue;
+                application = process;
+                break;
+            }
+            if (application != null && application.Responding)
+            {
+                application.Kill();
+                base.Uninstall(savedState);
+            }
+        }
+
+        private void showParameters()
+        {
+            StringBuilder sb = new StringBuilder();
+            StringDictionary myStringDictionary = this.Context.Parameters;
+            if (this.Context.Parameters.Count > 0)
+            {
+                foreach (string myString in this.Context.Parameters.Keys)
+                {
+                    sb.AppendFormat("String={0} Value= {1}\n", myString,
+                    this.Context.Parameters[myString]);
+                }
+            }
+            MessageBox.Show(sb.ToString());
+        }
+    }
+}
