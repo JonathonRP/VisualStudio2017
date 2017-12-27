1eb2a18088aa2275770f954f9d8d1be2352f3bf9
Unknown <jreesep@gmail.com>
2017-12-26 19:59:35 -0600

    should execute file in c:\

--- Database(Pokedex)/InstallerSetup/InstallerSetup1.cs
+++ Database(Pokedex)/InstallerSetup/InstallerSetup1.cs
@@ -42,7 +42,7 @@ namespace InstallerSetup
             {
                 if (Context.Parameters["DATABASECONNECTIONPROVIDER"] == "1")
                 {
-                    Process.Start(".\\AccessDatabaseEngine.exe");
+                    Process.Start("C:\\AccessDatabaseEngine.exe");
                 }
             }
             catch (Exception e)
